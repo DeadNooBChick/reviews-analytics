4ceee51c6ff17ab22b6c24face9acd6e85262e93
Kenneth Ku <kennethku324@gmail.com>
2021-10-14 14:38:23 +0800

    average letter version

--- read.py
+++ read.py
@@ -6,7 +6,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1 # count = count + 1
 		if count % 1000 == 0: # %係求餘數
 			print(len(data))
-print(data[0])
-print('-----------------------------')
-print(data[1])
-		
+print('檔案讀取完成, 總共有: ', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言的平均長度就是', sum_len/len(data))
